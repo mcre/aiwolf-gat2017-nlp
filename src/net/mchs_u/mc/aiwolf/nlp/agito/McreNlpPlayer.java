0b2be7df45912fb00d858b44841998cdd63cb421
mcre <mc@mchs-u.net>
2017-02-27 14:44:58 +0900

    サーバに接続後クライアント側から状況がわかりにくいので、状況表示を増やした

--- src/net/mchs_u/mc/aiwolf/nlp/agito/McreNlpPlayer.java
+++ src/net/mchs_u/mc/aiwolf/nlp/agito/McreNlpPlayer.java
@@ -1,7 +1,10 @@
 package net.mchs_u.mc.aiwolf.nlp.agito;
 
+import java.util.List;
+
 import org.aiwolf.common.data.Agent;
 import org.aiwolf.common.data.Player;
+import org.aiwolf.common.data.Talk;
 import org.aiwolf.common.net.GameInfo;
 import org.aiwolf.common.net.GameSetting;
 
@@ -12,6 +15,7 @@ public class McreNlpPlayer implements Player {
 	private GameInfo gameInfo = null;
 	private Mouth mouth;
 	private Ear ear;
+	private int printHead; // トークをどこまでprintしたかの管理
 
 	public McreNlpPlayer() {
 		player = new net.mchs_u.mc.aiwolf.curry.McrePlayer();
@@ -22,19 +26,28 @@ public class McreNlpPlayer implements Player {
 	public void update(GameInfo gameInfo) {
 		GameInfo prGameInfo = new GameInfoTranslater(gameInfo, ear);
 		player.update(prGameInfo);
+		
+		List<Talk> talkList = gameInfo.getTalkList();
+		List<Talk> prTalkList = prGameInfo.getTalkList();
+		
+		for(int i = printHead; i < talkList.size(); i++){
+			System.out.println("　○log : " + gameInfo.getAgent() + " " + getName() + "\t" + talkList.get(i) + " ( -> " + prTalkList.get(i).getText() + " ) ");
+			
+			printHead++;
+		}
 	}
 	
 	public String talk() {
 		String pr = player.talk();
 		String nl = mouth.toNaturalLanguageForTalk(gameInfo, pr);
-		System.out.println("☆ " + pr + " -> " + nl);
+		System.out.println("　●talk: " + gameInfo.getAgent() + " " + getName() + "\t" + nl + " ( <- " + pr + " ) ");
 		return nl;
 	}
 	
 	public String whisper() {
 		String pr = player.whisper();
 		String nl = mouth.toNaturalLanguageForWhisper(gameInfo, pr);
-		System.out.println("★ " + pr + " -> " + nl);
+		System.out.println("　●whis: " + gameInfo.getAgent() + " " + getName() + "\t" + nl + " ( <- " + pr + " ) ");
 		return nl;
 	}
 	
@@ -44,6 +57,7 @@ public class McreNlpPlayer implements Player {
 	}
 	
 	public void dayStart() {
+		printHead = 0;
 		player.dayStart();
 	}
 
